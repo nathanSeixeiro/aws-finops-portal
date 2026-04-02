9d9326a03f01c716ba0d94bf81a06a5bf93033b7
raphaelnc <raphaelnetto1@hotmail.com>
2026-04-02 10:11:50 -0300

    support for port 5501

--- cdk/stacks/api_stack.py
+++ cdk/stacks/api_stack.py
@@ -127,6 +127,7 @@ class ApiStack(Stack):
             rest_api_name="CostWatch API",
             default_cors_preflight_options=apigw.CorsOptions(
                 allow_origins=["http://localhost:5500", "http://127.0.0.1:5500",
+                               "http://localhost:5501", "http://127.0.0.1:5501",
                                "http://localhost:3000", "http://127.0.0.1:3000",
                                "http://localhost:8080", "http://127.0.0.1:8080"],
                 allow_methods=apigw.Cors.ALL_METHODS,
